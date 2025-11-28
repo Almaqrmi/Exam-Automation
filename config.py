57cb5b653ec6c4af8e3d853b2cb73ba95b3c69c3
mohammed almaqrmi <almaqrmi2@gmail.com>
2025-11-29 02:03:44 +0300

    change sqlite to postgresql DB

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
     
     # مسار قاعدة البيانات
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'instance', 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     # تعطيل تتبع التعديلات (لتحسين الأداء)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
