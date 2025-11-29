a01c606a863e65c07e62f5688ae23d06cb34eb23
mohammed almaqrmi <almaqrmi2@gmail.com>
2025-11-29 21:29:53 +0300

    update requirements

--- config.py
+++ config.py
@@ -14,12 +14,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://",
-            "postgresql+psycopg://",
-            1
-        )
-
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     
     # تعطيل تتبع التعديلات (لتحسين الأداء)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
