9c3c945eca4f4570980e5f49eea5c301f302d7de
mohammed almaqrmi <almaqrmi2@gmail.com>
2025-11-29 20:55:42 +0300

    update DB_URL

--- config.py
+++ config.py
@@ -12,6 +12,14 @@ class Config:
     
     # مسار قاعدة البيانات
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://",
+            "postgresql+psycopg://",
+            1
+        )
+
     
     # تعطيل تتبع التعديلات (لتحسين الأداء)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
